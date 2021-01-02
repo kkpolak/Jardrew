d325040e607acaaf2ec07bd50c3d55e9fe84f2ef
kacper.polak <kacper.polak@kalambagames.com>
2021-01-02 16:38:06 +0100

    first final version

--- src/main/java/com/example/kkpolak/Jardrew/JardrewApplication.java
+++ src/main/java/com/example/kkpolak/Jardrew/JardrewApplication.java
@@ -9,8 +9,6 @@ public class JardrewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JardrewApplication.class, args);
-		System.out.println("siema eniu");
-
 	}
 
 }
